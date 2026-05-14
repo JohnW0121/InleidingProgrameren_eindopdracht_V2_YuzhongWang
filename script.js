f0db289e1b6de7854f1bea4b9da5ab08c0ea91cc
JohnW0121 <yuzhong.wang@hva.nl>
2026-05-14 21:40:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,40 +13,46 @@ const nachtGeluid = new Audio("audio/night.mp3");
 let modus = "";
 
 // dag functie
-function dag(){
-dagGeluid.play();
-document.body.className = "dag";
-titel.textContent = "Goedemorgen!";
-tekst.textContent = "De zon schijnt.";
-modus = "dag";
+function dag() {
+    dagGeluid.play();
+    document.body.className = "dag";
+    titel.textContent = "Goedemorgen!";
+    tekst.textContent = "De zon schijnt.";
+    modus = "dag";
 }
 
 // nacht functie
-function nacht(){
-nachtGeluid.play();
-document.body.className = "nacht";
-titel.textContent = "Goedenavond!";
-tekst.textContent = "Slaap lekker.";
-modus = "nacht";
+function nacht() {
+    nachtGeluid.play();
+    document.body.className = "nacht";
+    titel.textContent = "Goedenavond!";
+    tekst.textContent = "Slaap lekker.";
+    modus = "nacht";
 }
 
 // reset met if/else
-function reset(){
-if(modus === "dag"){
-tekst.textContent = "Ben je moe?";
-}
-else if(modus === "nacht"){
-tekst.textContent = "Nog motivatie over?";
-}
-else{
-tekst.textContent = "Nog niets veranderd.";
-}
-document.body.className = "";
-titel.textContent = "Hallo!";
-modus = "";
+function reset() {
+
+    if (modus === "dag") {
+        tekst.textContent = "Ben je moe?";
+    }
+
+    else if (modus === "nacht") {
+        tekst.textContent = "Nog motivatie over?";
+    }
+
+    else {
+        tekst.textContent = "Nog niets veranderd.";
+    }
+
+    document.body.className = "";
+    titel.textContent = "Hallo!";
+    modus = "";
 }
 
 // events
 dagKnop.addEventListener("click", dag);
 nachtKnop.addEventListener("click", nacht);
 resetKnop.addEventListener("click", reset);
+
+//Bronne lijst
